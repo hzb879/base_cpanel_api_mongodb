8ee68c2f1b9072f25225e54333f1543d903db047
huangzebing <zebing.huang@spacewalker.com.cn>
2018-03-02 10:02:43 +0800

    待完善

---
+++ src/main/java/com/swk/cpanel/api/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package com.swk.cpanel.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Spring MVC 配置
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+//    private final Logger logger = LoggerFactory.getLogger(WebMvcConfig.class);
+    
+    @Value("${spring.profiles.active}")
+    private String env;//当前激活的配置文件
+
+
+    //解决跨域问题
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    //添加拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //接口签名认证拦截器，该签名认证比较简单，实际项目中可以使用Json Web Token或其他更好的方式替代。
+        if (!"dev".equals(env)) { //开发环境忽略签名认证
+          
+        }
+    }
+    
+}
